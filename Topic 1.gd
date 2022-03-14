d546beb338a73e7d2593c90d5770e56f8d148171
Ryan Cather <ryan.cather@ed.act.edu.au>
2022-03-15 09:44:08 +1100

    removed topic 1 scene

--- Topic 1.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-var simultaneous_scene = preload("res://Topic_1.tscn").instance()
-
-func _add_a_scene_manually():
-	# This is like autoloading the scene, only
-	# it happens after already loading the main scene.
-	get_tree().get_root().add_child(simultaneous_scene)
