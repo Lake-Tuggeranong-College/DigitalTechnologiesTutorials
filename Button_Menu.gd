54c25cfd0060f904bd5c49e9d250396ef3850ea4
Sam <87291184+Jasper-0195551@users.noreply.github.com>
2022-03-29 09:37:46 +1100

    int fix button same numbers

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -3,20 +3,21 @@ extends Button
 export(String) var scene_to_load
 
 
-func _on_Button3_pressed():
+
+
+
+
+
+func _on_Button2_pressed_Data_Quiz4():
 	Global.increaseScore()
 
 
-func _on_Button_pressed():
+func _on_Button4_pressed_Data_Quiz3():
 	Global.increaseScore()
 
 
-
-func _on_Button4_pressed():
+func _on_Button3_pressed_Data_Quiz2():
 	Global.increaseScore()
 
-
-
-func _on_Button2_pressed():
+func _on_Button_pressed_Data_Quiz1():
 	Global.increaseScore()
-
