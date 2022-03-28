707674e58e61e59d1e61d95b1a88886b50df61a7
Sam <87291184+Jasper-0195551@users.noreply.github.com>
2022-03-29 09:25:07 +1100

    int add score when correct answer has been pressed

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -5,3 +5,18 @@ export(String) var scene_to_load
 
 func _on_Button3_pressed():
 	Global.increaseScore()
+
+
+func _on_Button_pressed():
+	Global.increaseScore()
+
+
+
+func _on_Button4_pressed():
+	Global.increaseScore()
+
+
+
+func _on_Button2_pressed():
+	Global.increaseScore()
+
