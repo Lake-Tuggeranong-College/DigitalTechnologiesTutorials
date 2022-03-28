3541f9d5e262ac8c407d849a9ec15bec63f00cfc
Ryan Cather <ryan.cather@ed.act.edu.au>
2022-03-29 10:32:16 +1100

    included incorrect answer function

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -4,3 +4,8 @@ export(String) var scene_to_load
 
 func _on_Correct_Answer():
 	Global.increaseScore()
+	
+func _on_Incorrect_Answer():
+	# Created for future use
+	# To be completed at a later stage.
+	pass
