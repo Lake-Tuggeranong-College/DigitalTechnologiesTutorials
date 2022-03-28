a55f53fb6be9bcda9996847d12a8301678797b0a
Jackson <0706262@schoolsnet.act.edu.au>
2022-03-29 09:31:51 +1030

    Updated button points
    Changed the point scoring system so that there are no conflicts with other topics

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -3,5 +3,6 @@ extends Button
 export(String) var scene_to_load
 
 
-func _on_Button3_pressed():
-	Global.increaseScore()
+
+func _on_Button3_pressed_Software_Quiz1():
+	pass # Replace with function body.
