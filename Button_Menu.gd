c1ea4d1dc8f534bfb6723c14e020a24951149613
Rene Lam <87343026+MitchellNob@users.noreply.github.com>
2022-03-29 09:39:05 +1100

    Added my point scoring system
    Scoring should be working now, when you click on the correct buttons it adds 500 points

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -2,6 +2,17 @@ extends Button
 
 export(String) var scene_to_load
 
+func _on_Button4_pressed_code_quiz1():
+	Global.increaseScore()
 
-func _on_Button3_pressed():
+func _on_Button_pressed_code_quiz2():
+	Global.increaseScore()
+
+func _on_Button2_pressed_code_quiz3():
+	Global.increaseScore()
+
+func _on_Button4_pressed_code_quiz4():
+	Global.increaseScore()
+
+func _on_Button_pressed_code_quiz5():
 	Global.increaseScore()
