62525908c67b6d8a1e89b981cccaf8b547330b0b
0537982 <87291146+0537982@users.noreply.github.com>
2022-03-29 09:38:52 +1100

    Updated button/score code (Decisions)

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -3,5 +3,18 @@ extends Button
 export(String) var scene_to_load
 
 
-func _on_Button3_pressed():
+
+func _on_Button2_pressed_Decisions_Quiz1():
+	Global.increaseScore()
+
+func _on_Button4_pressed_Decisions_Quiz2():
+	Global.increaseScore()
+
+func _on_Button3_pressed_Decisions_Quiz3():
+	Global.increaseScore()
+
+func _on_Button2_pressed_Decisions_Quiz4():
+	Global.increaseScore()
+
+func _on_Button_pressed_Decisions_Quiz5():
 	Global.increaseScore()
