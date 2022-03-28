871319b665684a2ecd28c976fbc48bb88ded1c35
0706867 <0706867@schoolsnet.act.edu.au>
2022-03-29 09:36:02 +1100

    updated score scripts

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -3,5 +3,22 @@ extends Button
 export(String) var scene_to_load
 
 
-func _on_Button3_pressed():
+
+func _on_Button_pressed_NS_Q1():
+	Global.increaseScore()
+
+
+func _on_Button2_pressed_NS_Q2():
+	Global.increaseScore()
+
+
+func _on_Button3_pressed_NS_Q3():
+	Global.increaseScore()
+
+
+func _on_Button4_pressed_NS_Q4():
+	Global.increaseScore()
+
+
+func _on_Button4_pressed_NS_Q5():
 	Global.increaseScore()
