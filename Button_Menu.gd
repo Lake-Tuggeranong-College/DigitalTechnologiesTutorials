53dadb0e09b7df83e96918dbbd744ce58b7d90f4
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-04-05 19:38:52 +1000

    Adding sound effects to clicking on buttons

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -2,6 +2,8 @@ extends Button
 
 export(String) var scene_to_load
 
+var mouse_sound_effect = load("res://Music/Mouse Click - Sound Effect (HD).wav")
+
 func _on_Correct_Answer():
 	Global.increaseScore()
 	
@@ -9,3 +11,9 @@ func _on_Incorrect_Answer():
 	# Created for future use
 	# To be completed at a later stage.
 	pass
+
+
+func _on_Main_menu_pressed():
+	pass # Replace with function body.
+	$Music.stream = mouse_sound_effect
+	$Music.play()
