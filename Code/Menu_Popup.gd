6634ef587ae5f56214f8d87fd70cfd33ad9c04e3
Rene Lam <87343026+MitchellNob@users.noreply.github.com>
2022-04-05 10:25:47 +1000

    Added the Popup menu instantiated scene
    Still working on it so it isn't complete yet still need to make it so that when I press yes it changes scene, shouldn't mess any of the other scenes up

--- Code/Menu_Popup.gd
+++ Code/Menu_Popup.gd
@@ -1,16 +1,15 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.visible = 0
+
+func _on_No_pressed():
+	self.visible = 0
+
+func _on_Main_menu_pressed():
+	self.visible = 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Yes_pressed():
+	
