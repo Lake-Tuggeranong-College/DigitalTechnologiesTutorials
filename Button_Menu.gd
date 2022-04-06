75416a54ae5590ef5f2cf264026b12a4c2a307dc
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-04-06 10:37:45 +1000

    Music
    Lowered the volume of the music and attempted to add a mouse click sound effect

--- Button_Menu.gd
+++ Button_Menu.gd
@@ -2,11 +2,11 @@ extends Button
 
 export(String) var scene_to_load
 
-# var mouse_sound_effect = load("res://Music/Mouse Click - Sound Effect (HD).wav")
+var mouse_sound_effect = load("res://Music/Mouse Click - Sound Effect (HD).wav")
 
 func _on_Correct_Answer():
 	Global.increaseScore()
-	
+
 func _on_Incorrect_Answer():
 	# Created for future use
 	# To be completed at a later stage.
